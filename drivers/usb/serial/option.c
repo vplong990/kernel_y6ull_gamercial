ad929f5c6daa6dfa87053123254edcd415a37b90
vplong990 <vophilong990@gmail.com>
2024-10-14 15:22:32 +0700

    remove vendor id qualcom for test

--- drivers/usb/serial/option.c
+++ drivers/usb/serial/option.c
@@ -237,11 +237,13 @@ static void option_instat_callback(struct urb *urb);
 #define BANDRICH_PRODUCT_1011			0x1011
 #define BANDRICH_PRODUCT_1012			0x1012
 
-#define QUALCOMM_VENDOR_ID			0x05C6
+
+/*#define QUALCOMM_VENDOR_ID			0x05C6*/
 /* These Quectel products use Qualcomm's vendor ID */
 #define QUECTEL_PRODUCT_UC20			0x9003
 #define QUECTEL_PRODUCT_UC15			0x9090
 #define QUECTEL_PRODUCT_EC20			0x9215
+ 
 /* These u-blox products use Qualcomm's vendor ID */
 #define UBLOX_PRODUCT_R410M			0x90b2
 /* These Yuga products use Qualcomm's vendor ID */
